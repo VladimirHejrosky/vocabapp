f724712607f78a2f985f02462f8d0918af767ad4
Vladimir Hejrosky <vladimir.hejrosky@gmail.com>
2025-05-25 16:41:39 +0200

    fix some issues

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,7 @@ import { HyperText } from "@/components/magicui/hyper-text";
 import { SparklesText } from "@/components/magicui/sparkles-text";
 import { Language } from "@/lib/generated/prisma";
 import { cn } from "@/lib/utils";
-import {
-  FileText,
-  ShieldCheck
-} from "lucide-react";
+import { FileText, ShieldCheck } from "lucide-react";
 import { cookies } from "next/headers";
 import Link from "next/link";
 import LangSelector from "./components/LangSelector";
@@ -18,7 +15,7 @@ export default async function Home() {
     | undefined;
   return (
     <>
-      <div className="relative container mx-auto px-4 flex flex-col items-center mt-[5vh] min-h-[calc(100vh-9rem)] border-box text-center">
+      <div className="container mx-auto px-4 flex flex-col min-h-[calc(100vh-9rem)] border-box text-center">
         <div className="absolute inset-0 -z-10 w-full h-full overflow-hidden">
           <AnimatedGridPattern
             numSquares={30}
@@ -31,24 +28,26 @@ export default async function Home() {
             )}
           />
         </div>
-        <SparklesText sparklesCount={5} className="mb-4">
-          VocabApp
-        </SparklesText>
-        <HyperText className="text-xl text-muted-foreground mb-4">
-          Procvičuj jazykové znalosti a uč se nová slovíčka pomocí kartiček.
-        </HyperText>
-        <Menu />
-      </div>
+        <div className="flex flex-col items-center justify-center flex-grow">
+          <SparklesText sparklesCount={5} className="mb-4">
+            VocabApp
+          </SparklesText>
+          <HyperText className="text-xl text-muted-foreground mb-4">
+            Procvičuj jazykové znalosti a uč se nová slovíčka pomocí kartiček.
+          </HyperText>
+          <Menu />
+        </div>
 
-      <div className="absolute bottom-0 flex gap-4 p-4 mb-4 justify-between items-center w-full">
-        <LangSelector />
-        <div className="flex gap-4">
-          <Link href="/privacy-policy" title="Zásady ochrany osobních údajů">
-            <FileText className="h-5 w-5 text-muted-foreground hover:text-foreground transition-colors" />
-          </Link>
-          <Link href="/terms-of-service" title="Podmínky služby">
-            <ShieldCheck className="h-5 w-5 text-muted-foreground hover:text-foreground transition-colors" />
-          </Link>
+        <div className="flex gap-4 p-4 justify-between items-center w-full">
+          <LangSelector />
+          <div className="flex gap-4">
+            <Link href="/privacy-policy" title="Zásady ochrany osobních údajů">
+              <FileText className="h-5 w-5 text-muted-foreground hover:text-foreground transition-colors" />
+            </Link>
+            <Link href="/terms-of-service" title="Podmínky služby">
+              <ShieldCheck className="h-5 w-5 text-muted-foreground hover:text-foreground transition-colors" />
+            </Link>
+          </div>
         </div>
       </div>
     </>
